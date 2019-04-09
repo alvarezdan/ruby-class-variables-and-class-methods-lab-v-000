135140c1c0f256184628b673115807f82548f2ea
Daniel Alvarez <dnielalvrz@gmail.com>
2019-04-09 21:07:34 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -30,7 +30,7 @@ class Song
     genre_count = {}
     @@genres.each do |genre|
       if genre_count[genre]
-        genre_count[genre] +=1
+        genre_count[genre] += 1
       else
         genre_count[genre] = 1
       end
@@ -39,7 +39,15 @@ class Song
   end
 
   def self.artist_count
-  
+    artist_count = {}
+    @@artist.each do |artist|
+      if artist_count[artist]
+        artist_count[artist] += 1
+      else
+        artist_count[artist] = 1
+      end
+    end
+    artist_count
   end
 
 end
