d00f26e6dbe083006388c04a058b9af3cac22bf9
Daniel Alvarez <dnielalvrz@gmail.com>
2019-04-09 20:47:10 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,9 +3,9 @@ class Song
   @@count = 0
   attr_accessor :name, :artist, :genre
 
-  #def initialize
-  #  @@count += 1
-  #end
+  def initialize(name, artist, genre)
+    @@count += 1
+  end
 
 
 end
